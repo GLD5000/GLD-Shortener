b112448229b3834c581612e839d5cb1116321dcd
GLD5000 <garethlouisdevlin@gmail.com>
2023-06-28 13:12:53 +0100

    Added API endpoints and body parser

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -15,10 +16,15 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
 
+app.post('/api/shorturl/', (req, res) =>{
+  const input = req.body.url;
+  res.json({url: input});
+});
+app.get('/api/shorturl/:url', (req, res) =>{
+  res.json({url: req.params.url});
+
+});
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
